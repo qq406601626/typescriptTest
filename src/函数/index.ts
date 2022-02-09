49cf614489d467f24e2f29ecb66a9e85adfc4470
guoshuai <406601626@qq.com>
2022-02-09 09:59:51 +0800

    修改

--- src/函数/index.ts
+++ src/函数/index.ts
@@ -36,6 +36,7 @@ buildName('1', '2')
 // buildName('1',2) // error : restOfName为字符串数组
 
 // this参数
+// this参数是个假的参数，它出现在参数列表的最前面
 interface thiscanshu1 {
     prop1: string
     prop2: number
@@ -45,7 +46,32 @@ interface thiscanshu2 {
     prop1: string[]
     prop2: number[]
 
-    f1(): (prop1: number) => thiscanshu1
+    f1(this: thiscanshu2): () => thiscanshu1 // 在这里声明this类型为thiscanshu2
+}
+
+let thiscanshuInstance: thiscanshu2 = {
+    prop1: ['1', '2'],
+    prop2: [1, 2],
+    f1() {
+        return () => {
+            return {
+                prop1: this.prop1[0], // 在这里使用this时，会自动识别到thiscanshu2
+                prop2: this.prop2[0] // 在这里使用this时，会自动识别到thiscanshu2
+            }
+        }
+    }
+}
+
+// 重载
+// 函数重载只是为了校验参数的类型
+function chongzai(x: { suit: string, card: number }[]): number // 声明重载，但没有方法体
+function chongzai(x: number): { suit: string, card: number } // 声明重载，但没有方法体
+function chongzai(x):any{ // 声明函数，包含具体方法体。声明的参数不能写参数类型，返回值要为any
+    if (typeof x == 'object') {
+        return 1
+    } else if (typeof x == 'number') {
+        return {suit: '1', card: 1}
+    }
 }
 
 
