c2d89396602c390c929c1bb2fbd5516d657cb839
guoshuai <406601626@qq.com>
2022-03-31 11:05:18 +0800

    修改

--- src/test.ts
+++ src/test.ts
@@ -99,3 +99,12 @@
 // }
 // type bbb = aaa[keyof aaa]
 
+
+// type aaaa<T> = T extends Array<any> ? T[number] : T
+// type bbbb = aaaa<Array<string>>
+
+
+
+type aaa<T> = T extends Array<infer U> ? U : T // 判断T是否extends Array，如果是，则根据Array<string> 判断Array<infer U>中的U类型。这里U为string
+type bbb = aaa<Array<string>>
+
